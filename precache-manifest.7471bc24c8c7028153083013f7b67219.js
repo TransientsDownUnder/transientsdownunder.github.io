9dea631c670eb9b143b0ece3b101624eb3beb030
Nicolas Rodriguez-Segovia <nicolas@pop-os.localdomain>
2023-02-21 11:06:13 +1100

    Updates

---
+++ precache-manifest.7471bc24c8c7028153083013f7b67219.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "af4e1a5170174c77c1e8",
+    "url": "/transientsdownunder2024/static/js/main.af4e1a51.chunk.js"
+  },
+  {
+    "revision": "b5c7b4a6d0e36ca53157",
+    "url": "/transientsdownunder2024/static/css/1.33ca96d2.chunk.css"
+  },
+  {
+    "revision": "b5c7b4a6d0e36ca53157",
+    "url": "/transientsdownunder2024/static/js/1.b5c7b4a6.chunk.js"
+  },
+  {
+    "revision": "898dbbbfc561b90c94ed",
+    "url": "/transientsdownunder2024/static/js/runtime~main.898dbbbf.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/transientsdownunder2024/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "4cb9e4a324c6803f7ae1d160a712bbd1",
+    "url": "/transientsdownunder2024/static/media/hero-light.4cb9e4a3.jpg"
+  },
+  {
+    "revision": "7e51560406db2f316b9b33272df6954e",
+    "url": "/transientsdownunder2024/static/media/eso1033a.7e515604.jpg"
+  },
+  {
+    "revision": "ff61d2860bde30a53cf695af9664ddd7",
+    "url": "/transientsdownunder2024/static/media/GH.ff61d286.svg"
+  },
+  {
+    "revision": "8d4235ad4a515f292e4bfc9fc4cc14b8",
+    "url": "/transientsdownunder2024/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5bc85a9c5490b6ff815a899e664f83c4.js"
+  "/transientsdownunder2024/precache-manifest.7471bc24c8c7028153083013f7b67219.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/transientsdownunder2024/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
